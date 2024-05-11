40445c7f4b616ae02296f438d05891cd0d4a6e25
Aurea <94511889+Aurea6@users.noreply.github.com>
2024-05-11 21:38:56 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ youtubeVisitAllowed: false });
+    chrome.storage.local.set({ gluoVisitAllowed: false });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab && tab.url) {
-        chrome.storage.local.get('youtubeVisitAllowed', (result) => {
-            if (!tab.url.includes('youtube.com') && !tab.url.includes('youtu.be')) return;
+        chrome.storage.local.get('gluoVisitAllowed', (result) => {
+            if (!tab.url.includes('gluo.xyz') && !tab.url.includes('gluo.xyz')) return;
 
-            const youtubeVisitAllowed = result.youtubeVisitAllowed || false;
+            const gluoVisitAllowed = gluoVisitAllowed || false;
 
-            if (!youtubeVisitAllowed) {
+            if (!gluoVisitAllowed) {
                 chrome.tabs.update(tabId, { url: chrome.runtime.getURL('warning.html') });
             }
         });
@@ -17,9 +17,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    chrome.tabs.query({ url: '*://*.youtube.com/*' }, (tabs) => {
+    chrome.tabs.query({ url: '*://*.gluo.xyz/*' }, (tabs) => {
         if (tabs.length === 0) {
-            chrome.storage.local.set({ youtubeVisitAllowed: false });
+            chrome.storage.local.set({ gluoVisitAllowed: false });
         }
     });
 });
